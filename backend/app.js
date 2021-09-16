a87e0fc6d775b44cb5b0162a1028c62bcd08a8a1
yogesh <etpr.yogesh@gmail.com>
2021-09-16 23:35:39 +0530

    app.js file where all the middleware are established and connected to app

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Setup Cross Origin
+app.use(require("cors")());
+
+//Bring in the routes
+app.use("/user", require("./routes/user"));
+app.use("/product", require("./routes/product"));
+app.use("/:_id/survey", require("./routes/survey"));
+
+
+//Setup Error Handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
+
+module.exports = app;
